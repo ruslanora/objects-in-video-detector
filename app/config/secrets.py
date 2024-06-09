6fb824ea62132d2e616671c69fed261b57ef9792
Ruslan Ora <ruslan.ora@outlook.com>
2024-06-09 13:09:47 -0400

    Create /vision endpoint, accept file upload

--- app/config/secrets.py
+++ app/config/secrets.py
@@ -15,3 +15,12 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # Load environment variables from .env
 load_dotenv(Path(BASE_DIR).joinpath(".env"))
+
+file_formats = os.environ.get("ACCEPT_FILE_FORMATS", None)
+
+if not file_formats:
+    ACCEPT_FILE_FORMATS = ["mp4"]
+else:
+    ACCEPT_FILE_FORMATS = file_formats.split(";")
+
+VIDEO_DURATION_LIMIT = int(os.environ.get("VIDEO_DURATION_LIMIT", "300"))
--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@
 Entry point to the project.
 """
 
-from fastapi import FastAPI
+import tempfile
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from moviepy import VideoFileClip
+
+from app.config.secrets import ACCEPT_FILE_FORMATS, VIDEO_DURATION_LIMIT
+from app.utils.logger import logger
 
 
 app = FastAPI(
@@ -19,3 +26,37 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.post("/vision")
+async def process(
+    file: UploadFile = File(...),
+) -> JSONResponse:
+    # Content type validation
+    if not file.content_type.startswith("video/"):  # type: ignore[union-attr]
+        raise HTTPException(status_code=400, detail="Invalid content type")
+
+    # Validate file format
+    file_format = file.filename.split(".")[-1]  # type: ignore[union-attr]
+
+    if file_format not in ACCEPT_FILE_FORMATS:
+        detail = f"Supported files: {", ".join(ACCEPT_FILE_FORMATS)}"
+        raise HTTPException(status_code=400, detail=detail)
+
+    with tempfile.NamedTemporaryFile(
+        delete=False, suffix=file.filename.lower()  # type: ignore[union-attr]
+    ) as temp:
+        temp.write(await file.read())
+        pathname = temp.name
+
+    video = VideoFileClip(pathname)
+
+    if video.duration > VIDEO_DURATION_LIMIT:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Too long. Limit is {VIDEO_DURATION_LIMIT} seconds",
+        )
+
+    logger.info("The vide0 file is valid, starting AI processing...")
+
+    return JSONResponse(status_code=200, content={"message": "OK"})
